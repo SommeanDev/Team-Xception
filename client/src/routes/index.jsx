0de51438e5143079b3395df1e7b32ce929c545d9
Ketan Pardhi <ketanpardhi7@gmail.com>
2025-02-28 14:31:40 +0530

    add create post

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
 import ViewPost from "../components/ViewPost";
+import CreatePost from "../pages/CreatePost";
 
 
 const router=createBrowserRouter([
@@ -17,6 +18,10 @@ const router=createBrowserRouter([
                 path:"/posts/:id",
                 element:<ViewPost/>
             },
+            {
+                path:"/create",
+                element:<CreatePost/>
+            }
         ]
     }
 ])
