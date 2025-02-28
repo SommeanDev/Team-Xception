1bb9ca1339f73f6716a01059e0f78e27adaf6048
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-02-28 11:10:17 +0530

    add routing and context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes'
+import { AppContext } from './context/AppContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppContext>
+      <RouterProvider router={router} />
+    </AppContext>
   </StrictMode>,
 )
